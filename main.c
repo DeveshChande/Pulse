cbd2965bac6f19df2f76e89b39ca9a6926a57632
Devesh Chande <33889891+DeveshChande@users.noreply.github.com>
2023-08-20 17:25:32 -0400

    Refactored execution flow.
    Pulse now parses options in a coherent manner and decides the execution flow accordingly.

--- main.c
+++ main.c
@@ -1,35 +1,117 @@
-#include <dirent.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include "pe.h"
-
-struct switchList{
-    bool fileSpecified;
-    char* pathOfFileSpecified;
-    bool directorySpecified;
-    char* pathOfDirectorySpecified;
-    bool exportResult;
-} globalSwitchList;
-
+#include <headers/pe32.h>
 
 int main(int argc, char* argv[]){
     clock_t t = clock();
+    struct switchList* psList = malloc(sizeof(struct switchList));
+    
+    if(psList != NULL){
+    	psList->fileSpecified = true;
+    	psList->pathOfFileSpecified = "/test/samples/ChromeSetup.exe";
+    	psList->directorySpecified = true;
+    	psList->pathOfDirectorySpecified = "/test/samples/";
+    	psList->exportResult = false;
+    	psList->extractResources = false;
+    	psList->extractStrings = false;
+    	psList->vtLookup = false;
+    	psList->urlLookup = false;
+    	psList->shodanLookup = false;
+    	psList->dnsLookup = false;
+    	psList->computeHash = false;
+    	psList->capabilityDetection = false;
+    }
+    else{
+    	perror("Failed to allocate memory for switchList");
+    	return -1;
+    }
+    
 
-    for(int argCount = 1; argCount < argc; argCount+=2){
-        if(!strcmp(argv[argCount], "-f")){
-            globalSwitchList.fileSpecified = true;
-            globalSwitchList.pathOfFileSpecified = argv[argCount+1];
+    size_t argCount = 0;
+    for(argCount=1; argCount<argc; argCount++){
+        if(!strcmp(argv[argCount], "--path")){
+            psList->fileSpecified = true;
+            psList->pathOfFileSpecified = argv[argCount+1];
+            argCount++;
         }
-    }
 
-    if(globalSwitchList.fileSpecified){
-        parsePE(globalSwitchList.pathOfFileSpecified, globalSwitchList.exportResult);
-    }
+        if(!strcmp(argv[argCount], "--directory")){
+            psList->directorySpecified = true;
+            psList->pathOfDirectorySpecified = argv[argCount+1];
+            argCount++;
+        }
+        
+        if(!strcmp(argv[argCount], "--exportResult"))
+            psList->exportResult = true;
 
+        if(!strcmp(argv[argCount], "--extractResources"))
+            psList->extractResources = true;
+
+        if(!strcmp(argv[argCount], "--extractStrings"))
+            psList->extractStrings = true;
+
+        if(!strcmp(argv[argCount], "--vtLookup"))
+            psList->vtLookup = true;
+
+        if(!strcmp(argv[argCount], "--urlLookup"))
+            psList->urlLookup = true;
+
+        if(!strcmp(argv[argCount], "--shodanLookup"))
+            psList->shodanLookup = true;
+
+        if(!strcmp(argv[argCount], "--dnsLookup"))
+            psList->dnsLookup = true;
+
+        if(!strcmp(argv[argCount], "--computeHash"))
+            psList->computeHash = true;
+
+        if(!strcmp(argv[argCount], "--capabilityDetection"))
+            psList->capabilityDetection = true;
+    }
+    
+ 
+    if(!psList->fileSpecified){
+    	perror("No file specified. Terminating.\n");
+    	return -1;
+    }
+    	
+    
+    FILE* pefile = fopen(psList->pathOfFileSpecified, "rb");
+    
+    if(pefile != NULL){
+        uint16_t* WORD_Buffer = calloc(2, sizeof(uint8_t));
+        uint32_t* DWORD_Buffer = calloc(4, sizeof(uint8_t));
+        uint32_t elfanew = convert_DWORD_To_uint32_t(readDWord(pefile, 0x3C, DWORD_Buffer));
+        uint16_t trueMagic = reverse_endianess_uint16_t(readWord(pefile, elfanew+24, WORD_Buffer));
+
+        switch(trueMagic){
+            case 0x10b:
+                parsePE32(pefile, psList);
+                break;
+            case 0x20b:
+                printf("Write parsePE64().\n"); //parsePE64(pefile, psList);
+                break;
+            default:
+                printf("ERROR: Unsupported type of image.\n\n");
+                break;
+        }
+
+        free(WORD_Buffer);
+        free(DWORD_Buffer);
+    }  
+    else{
+        perror("Failed call to fopen()");
+        return -1;
+    }
+    
+    fclose(pefile);
+    free(psList);
+    
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
     printf("\n\nParsing took %f seconds to execute.\n", time_taken);
-    return 0;    
+
+    return 0;
 }
